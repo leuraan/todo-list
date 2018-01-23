f535cb130c5b4db61744327b9768ecdb73845e9f
Leuran <leuran@msn.com>
2018-01-23 16:04:16 +0100

    Express.js app created

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+// init app
+const app = express();
+
+//routes
+app.get('/', function(req, res) {
+  res.send("Index page");
+});
+
+//running app
+
+app.listen(3000, function() {
+  console.log("App running at http://localhost:3000");
+});
