fc977cc0f22639f5240664c8aa7dcf42323fcb54
Leuran <leuran@msn.com>
2018-01-24 14:19:01 +0100

    crud routes created, from frontedn added

--- app.js
+++ app.js
@@ -3,9 +3,57 @@ const express = require('express');
 // init app
 const app = express();
 
+// Views location
+
+app.set('views', __dirname + '/views');
+
+// Setting template engine
+app.set('view engine', 'ejs');
+
+// Setup MongoDB
+
+const MongoClient = require('mongodb').MongoClient;
+const mongoURL = 'mongodb://localhost:27017/todolist';
+
+// Connecting to MongoDB
+
+MongoClient.connect(mongoURL, function(err, db){
+  if (err) {
+    console.log(err);
+  } else {
+
+  console.log('Database connected succesfully!');
+    todos = db.collection('todos');
+  }
+});
+
 //routes
 app.get('/', function(req, res) {
-  res.send("Index page");
+  res.render("index", {name: "Faqja fillestare", title: "Index"});
+});
+
+app.get('/', function(req, res){
+  res.render('index');
+});
+
+app.get('/todos:id', function(req, res){
+  res.render('show');
+});
+
+app.post('/todos/add', function(req, res){
+  res.redirect("/");
+});
+
+app.get('/todos/edit/:id', function(req, res){
+  res.render('edit');
+});
+
+app.post('/todos/update/:id', function(req, res){
+  res.redirect("/");
+});
+
+app.get('/todos/delete/:id', function(req, res){
+  res.redirect("/");
 });
 
 //running app
